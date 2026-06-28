f00b90f8869056ab14a4f2329ecb37e7d2bfc230
Bhavesh Gadre <gadrebhawesh123@gmail.com>
2026-06-28 23:17:34 +0530

    Add Node.js Employee Management API

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Employee Management API is Running 🚀");
+});
+
+app.get("/health", (req, res) => {
+    res.send("API Status: Healthy");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
